1ecbcd3902a782dfac3aa05862372b98c6b09ebe
shirishavalluri <47477210+shirishavalluri@users.noreply.github.com>
2019-11-23 13:57:10 +0530

    Test Jupyter Notebook

---
+++ Python Fundamentals.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+type(12.5)
+
+
+# In[3]:
+
+
+print("This will be printed")
+
+print("This will NOT be printed")
+
+
+# In[5]:
+
+
+type('Shirisha')
+
+
+# In[6]:
+
+
+type(12.99)
+
+
+# In[9]:
+
+
+sys.float_info
+
+
+# In[12]:
+
+
+import sys
+sys.float_info
+
+
+# In[ ]:
+
+
+
+
