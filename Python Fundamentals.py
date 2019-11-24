ae103941aad9851d11dc9d51ba9d56f3e0bbf107
shirishavalluri <47477210+shirishavalluri@users.noreply.github.com>
2019-11-24 13:16:10 +0530

    String operations

--- Python Fundamentals.py
+++ Python Fundamentals.py
@@ -40,8 +40,127 @@ import sys
 sys.float_info
 
 
+# In[10]:
+
+
+25/6 # difference between / and // exact result (float)
+
+
+# In[9]:
+
+
+25//6 # // for result round off 
+
+
+# In[15]:
+
+
+3* "Shirisha "
+
+
 # In[ ]:
 
 
+#  \n - it displays a new line \t represents a tab 
 
+
+# In[24]:
+
+
+b= 'shirisha'
+
+
+# In[22]:
+
+
+a =  b.upper()
+
+
+# In[25]:
+
+
+a
+
+
+# In[1]:
+
+
+name = 'Shirisha Valluri'
+
+
+# In[2]:
+
+
+name
+
+
+# In[5]:
+
+
+print(name[0])# indexing starts at 0 not 1
+
+
+# In[6]:
+
+
+name.find('Val')
+
+
+# In[7]:
+
+
+New_Name = name.replace('Valluri','Prasad')
+New_Name
+
+
+# In[10]:
+
+
+D = "Shirisha"
+D
+print(D[:3])
+
+
+# In[11]:
+
+
+print(D[::2])
+
+
+# In[16]:
+
+
+F = "You are wrong"
+
+
+# In[18]:
+
+
+G= F.upper()
+G
+
+
+# In[21]:
+
+
+print (r"\")
+
+
+# In[22]:
+
+
+G = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
+G
+
+
+# In[24]:
+
+
+F = G.replace('Mary','Shirisha')
+
+
+# In[25]:
+
+
+F
 
